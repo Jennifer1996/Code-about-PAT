b61dac0a0c179152d5bed679aa0db2bc70175ff6
Jennifer1996 <cs305602115@163.com>
2018-02-11 15:22:43 +0800

    Update

---
+++ exercise/main.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+//    int a,b;
+//    scanf("%d%d",&a,&b);
+//    printf("%d",a+b);
+//    int a=1,b=2;
+//    printf("%d",a+b);
+//    double a=3.14,b=0.12;
+//    double c=a+b;
+//    printf("%f",c);
+//    char c1='z',c2='j',c3=117;
+//    printf("%c%c%c",c1,c2,c3);
+//    int num1=1,num2=2;
+//    printf("%d\n\n%d",num1,num2);
+//    printf("%c",7);
+//    char str1[25]="Wo ai de ren bu ai wo";
+//    char str2[25]="so sad a story it is.";
+//    printf("%s,%s",str1,str2);
+    bool flag1=0,flag2=true;
+    int a=1,b=1;
+    printf("%d %d %d\n",flag1,flag2,a==b);
+    return 0;
+}
+
